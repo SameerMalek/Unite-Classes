fec77f80a11ac5d817a79e338847fa8037c1b13e
Mohammad Sameer Malek <maleksameer540@gmail.com>
2025-01-10 16:55:04 -0700

    App.js modified

--- Frontend/unite-classes/src/App.js
+++ Frontend/unite-classes/src/App.js
@@ -4,17 +4,21 @@ import HomePage from './Pages/homePage/HomePage.jsx';
 import ClassDetailPage from './Pages/classDetailPage/ClassDetailPage';
 import CourseDetailPage from './Pages/courseDetailPage/CourseDetailPage';
 import UploadContentPage from './Pages/uploadContentPage/UploadContentPage';
+import Header from './Components/header/header';
+import Footer from './Components/footer/Footer';
 
 function App() {
   return (
     <Router>
       <div className="container">
+        <Header/>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/class/:classId" element={<ClassDetailPage />} />
           <Route path="/course/:courseId" element={<CourseDetailPage />} />
           <Route path="/upload/:courseId" element={<UploadContentPage />} />
         </Routes>
+        <Footer/>
       </div>
     </Router>
   );
