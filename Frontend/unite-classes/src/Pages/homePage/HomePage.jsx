62e0a10746ab3419d9dc542db700b9a98523f7b6
Shakil Malek <maleksameer528@gmail.com>
2025-01-12 10:23:57 +0530

    minor change

--- Frontend/unite-classes/src/Pages/homePage/HomePage.jsx
+++ Frontend/unite-classes/src/Pages/homePage/HomePage.jsx
@@ -6,11 +6,14 @@ const HomePage = () => {
     { id: 6, name: "Class 6" },
     { id: 7, name: "Class 7" },
     { id: 8, name: "Class 8" },
+    { id: 9, name: "Class 9" },
+    { id: 10, name: "Class 10" }
+
   ];
 
   return (
     <div>
-      <h1>Welcome to Tuition Classes</h1>
+      <h1>Welcome to Unite Classes</h1>
       <div className="card-container">
         {classes.map((cls) => (
           <Link key={cls.id} to={`/class/${cls.id}`} className="card">
